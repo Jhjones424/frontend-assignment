736faaf3208e757308ce34d616ceebdb912f7ed1
Jay <jhjones424@gmail.com>
2021-08-04 19:28:48 -0700

    Initial setup complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,51 @@
-import logo from './logo.svg';
+import logo from '../images/logo.svg';
+import React from 'react';
+import styled from 'styled-components';
+import MovieGrid from './MovieGrid.js';
+import axios from 'axios';
 
-const App = () => (
-	<img src={logo} alt="Timescale" />
-)
+const Header = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+`
+
+const Container = styled.div`
+	margin-left: 7%;
+	margin-right: 7%;
+`
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			allMovies : [],
+			displayed : []
+		}
+	}
+
+	componentDidMount() {
+		let currDate = new Date().toISOString();
+		console.log('currDate:', currDate);
+		let endpoint = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_MOVIE_DB_API_KEY}&sort_by=primary_release_date.desc&primary_release_date.lte=${currDate}`
+		axios.get(endpoint)
+			.then(data => console.log('data:', data))
+	}
+
+	render () {
+		return (
+			<Container>
+				<Header>
+					<img src={logo} alt="Timescale" />
+					<div>Search Bar</div>
+				</Header>
+				<hr></hr>
+				<div> Most Recent Movies</div>
+				<MovieGrid movies={this.state.displayed}/>
+			</Container>
+		)
+	}
+
+}
 
 export default App;
---
+++ src/components/MovieGrid.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const MovieGrid = (props) => {
+
+  return (
+    <div>Movie Grid</div>
+    // {props.movies.map((movie, i) => (
+    //   <div>{movie}</div>
+    //   // <Movie movie={movie}/>
+    //   ))
+    // }
+  )
+}
+
+export default MovieGrid;
